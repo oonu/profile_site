77dc3d662e47ec43f8caa32c9ac9ffb57ddb8b03
Oonu <yatsu@oonu.jp>
2014-02-06 23:58:17 +0900

    agent param updated

--- db/migrate/20140202111349_create_agents.rb
+++ db/migrate/20140202111349_create_agents.rb
@@ -7,21 +7,38 @@ class CreateAgents < ActiveRecord::Migration
       # 名前
       t.string :first_name,      null: false
       t.string :first_name_kana, null: false
+
+      # 生年月日
+      t.date :birth_day
       # 性別
       t.integer :sex
       # 年齢
       t.integer :age
+
       # 出身地
       t.string :home_city
-      # 所在地
+      # 居住地
       t.string :location
-      # アピールポイント
+
+      # 趣味
+      t.string :hobby
+      # 特技
+      t.string :ability
+
+      # 一言
+      t.string :free_word
+      # アピール
       t.string :appeal
-      # ポイント
-      t.integer :point
+
       # 画像
       t.binary :icon, limit: 10.megabyte
       t.string :icon_content_type
+
+      # メールアドレス
+      t.string :email
+
+      # ポイント
+      t.integer :point
 
       t.timestamps
     end
