9555c47e1c3066898894abe18400cc1c1aed99f2
Oonu <yatsu@oonu.jp>
2014-02-11 18:53:24 +0000

    add agent_id for businesses

--- db/migrate/20140206150944_create_businesses.rb
+++ db/migrate/20140206150944_create_businesses.rb
@@ -1,11 +1,13 @@
 class CreateBusinesses < ActiveRecord::Migration
   def change
     create_table :businesses do |t|
+      t.integer :agent_id
       t.string :outline
       t.string :company_name
       t.string :company_position
 
       t.timestamps
     end
+    add_index :businesses, :agent_id
   end
 end
